3425c390398c380b16f148763cf141b271ded395
fbjwying2 <35253292+fbjwying2@users.noreply.github.com>
2018-04-09 21:21:37 +0800

    Update train.py

--- train.py
+++ train.py
@@ -18,20 +18,21 @@ FLAGS, unparsed = parse_args()
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s', level=logging.DEBUG)
 
+cwdir = os.getcwd() + '/'
 
-vocabulary = read_data(FLAGS.text)
+vocabulary = read_data(cwdir + FLAGS.text)
 print('Data size', len(vocabulary))
 
 
-with open(FLAGS.dictionary, encoding='utf-8') as inf:
+with open(cwdir + FLAGS.dictionary, encoding='utf-8') as inf:
     dictionary = json.load(inf, encoding='utf-8')
 
-with open(FLAGS.reverse_dictionary, encoding='utf-8') as inf:
+with open(cwdir + FLAGS.reverse_dictionary, encoding='utf-8') as inf:
     reverse_dictionary = json.load(inf, encoding='utf-8')
 
 
 model = Model(learning_rate=FLAGS.learning_rate, batch_size=FLAGS.batch_size, num_steps=FLAGS.num_steps)
-model.build('./data/embedding.npy')
+model.build(cwdir + 'data/embedding.npy')
 
 
 with tf.Session() as sess:
