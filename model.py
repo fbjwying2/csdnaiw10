cb562b5dda6dcfc50547af2f8cb58b522af44f32
fbjwying2 <35253292+fbjwying2@users.noreply.github.com>
2018-04-10 01:17:20 +0800

    Update model.py

--- model.py
+++ model.py
@@ -74,7 +74,7 @@ class Model():
             cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * self.rnn_layers, state_is_tuple=True)
             cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=self.keep_prob)
 
-            self.state_tensor = cell.zero_state(self.state_size, tf.float32)
+            self.state_tensor = cell.zero_state(self.batch_size, tf.float32)
             
             self.outputs_state_tensor, _state_tensor = tf.nn.dynamic_rnn(cell, rnn_inputs, initial_state=self.state_tensor)
 
